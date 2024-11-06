3f3aa1c89191896c0d3018f8b94fc966c5ee9c27
leo <lch01234@gmail.com>
2024-11-06 18:18:04 +0900

    test http

--- uniclipboard/src/main.cpp
+++ uniclipboard/src/main.cpp
@@ -1,7 +1,35 @@
+#include <unistd.h>
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
+#include "httplib.h"
+
+void test_http() {
+  // HTTP
+  httplib::Client cli("34.226.255.154:80");
+
+  // HTTPS
+  // httplib::Client cli("https://cpp-httplib-server.yhirose.repl.co");
+
+  cli.set_connection_timeout(5);
+  while (1) {
+    printf("access server \n");
+    try {
+      auto res = cli.Get("/index.html");
+      if (res == nullptr) {
+        printf("failed\n");
+        continue;
+      }
+      std::cout << "status:" << res->status << std::endl;
+      // std::cout << "body" << res->body << std::endl;
+    } catch (const std::exception& e) {
+      std::cout << e.what();
+    }
+    sleep(3);
+  }
+}
 
 int main(int argc, char* argv[]) {
-  std::cout << "starting..." << std::endl;
+  printf("starting...\n");
+  test_http();
 }
